571d297eab94925d8dfe3eab2aeb4ace8322e348
SehrishRehman <sehrishrehman527@gmail.com>
2025-05-21 06:27:29 -0700

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-// next.config.js
-module.exports = {
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-}
-
+};
 
 export default nextConfig;
